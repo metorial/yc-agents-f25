f2e13d5d7a6e05faf4abb6c8b6a5fde9e9e6b0e8
Tobias Herber <22559657+herber@users.noreply.github.com>
2025-11-01 10:10:34 -0700

    Fix submit

--- src/app/submit/route.tsx
+++ src/app/submit/route.tsx
@@ -5,7 +5,7 @@ export const runtime = 'edge';
 export let GET = async (req: NextRequest) => {
   let authUrl = new URL('https://auth.metorial.com/oauth/authorize');
   authUrl.searchParams.set('client_id', process.env.OAUTH_CLIENT_ID!);
-  authUrl.searchParams.set('redirect_uri', `${req.nextUrl.origin}/apply/callback`);
+  authUrl.searchParams.set('redirect_uri', `${req.nextUrl.origin}/submit/callback`);
   authUrl.searchParams.set('response_type', 'code');
   authUrl.searchParams.set('state', Math.random().toString(36).substring(2, 15));
 
